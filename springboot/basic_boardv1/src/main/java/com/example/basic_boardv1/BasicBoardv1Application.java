4bad41b09270dab961dd6193d29ad51b9feea4c9
hankookkim <nahmik84@gmail.com>
2025-02-25 11:13:12 +0900

    Prj setup

---
+++ springboot/basic_boardv1/src/main/java/com/example/basic_boardv1/BasicBoardv1Application.java
@@ -0,0 +1,13 @@
+package com.example.basic_boardv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBoardv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBoardv1Application.class, args);
+	}
+
+}
---
+++ springboot/basic_boardv1/src/test/java/com/example/basic_boardv1/BasicBoardv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_boardv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBoardv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

91809cda4c8627520264462b694d6f5934f8d615
hankookkim <nahmik84@gmail.com>
2025-03-09 19:31:06 +0900

    p

--- springboot/essentials/src/main/java/com/example/essentials/filter/LoggingFilter.java
+++ springboot/essentials/src/main/java/com/example/essentials/filter/LoggingFilter.java
@@ -6,11 +6,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-//@Component
-public class LoggingFilter implements Filter {
+
+public class LoggingFilter implements Filter{
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        //필터 초기화 (필요시)
     }
 
     @Override
@@ -18,26 +18,20 @@ public class LoggingFilter implements Filter {
                          ServletResponse servletResponse,
                          FilterChain filterChain)
             throws IOException, ServletException {
-        //**필수 (http에 맞게 구현)
 
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletResponse response= (HttpServletResponse) servletResponse;
 
+        System.out.println("Request URI"+request.getRequestURI());
+        System.out.println("Request Method"+request.getMethod());
 
-        //--->요청정보 로깅
-        System.out.println("Request URI:" + request.getRequestURI());
-        System.out.println("Request Method:" + request.getMethod());
+        filterChain.doFilter(servletRequest,servletResponse);
 
-        //필터체인 :계속해서 다음 필터 또는 서블릿으로 전달
-        filterChain.doFilter(servletRequest, servletResponse);
-
-        //<---- 응답 상태 코드 로깅
-        System.out.println("Response Status:" + response.getStatus());
-
+        System.out.println("Reponse Status"+response.getStatus());
     }
 
     @Override
     public void destroy() {
-       //필터 종료시 처리(필요시)
+
     }
 }
